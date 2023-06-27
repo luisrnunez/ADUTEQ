3a2ac6471879ec6bc9c4da83cc48232f4631bac1
William-Vera <william.vera2018@uteq.edu.ec>
2023-06-27 22:42:34 +0200

    modulo_proveedores

---
+++ Aduteq/proveedores/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Proveedor(models.Model):
+    nombre=models.CharField(max_length=200)
+    telefono=models.CharField(max_length=13)
+    RUC=models.CharField(max_length=13)
+    direccion=models.CharField(max_length=500)
+    comision=models.DecimalField(max_digits = 8, decimal_places = 2)
+    estado=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre
---
+++ Aduteq/socios/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.login_),
+    path('principal/', views.Principal, name='principal'),
+    path('login/', views.Autenticacion_usuarios),
+    path('recuperacion/', views.Recuperar_cuenta),
+    path('socios/', views.ListaSocios, name='listar_socios'),
+    path('socios/agg/', views.AggSocio),
+    path('guardar_socio/', views.guardar_socio, name='guardar_socio'),
+    path('editar_socio/<int:socio_id>/',views.editar_socio, name='editar_socio'),
+    path('eliminar_socio/<int:socio_id>/', views.eliminar_socio, name='eliminar_socio'),
+]
