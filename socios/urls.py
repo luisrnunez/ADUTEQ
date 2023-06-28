58b57ba9793ef6773b316eee2a0f19b8486d7b7a
luis <lnunezr@uteq.edu.ec>
2023-06-27 22:08:56 -0500

    BETA3

--- socios/urls.py
+++ socios/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('login/', views.Autenticacion_usuarios),
     path('recuperacion/', views.Recuperar_cuenta),
     path('socios/', views.ListaSocios, name='listar_socios'),
-    path('socios/agg/', views.AggSocio),
+    path('socios/agg/', views.AggSocio, name='agg_socios'),
     path('guardar_socio/', views.guardar_socio, name='guardar_socio'),
     path('editar_socio/<int:socio_id>/',views.editar_socio, name='editar_socio'),
     path('eliminar_socio/<int:socio_id>/', views.eliminar_socio, name='eliminar_socio'),
+    path('buscar_socios/', views.buscar_socios, name='buscar_socios'),
 ]
--- socios/views.py
+++ socios/views.py
@@ -7,15 +7,15 @@ from django.contrib import messages
 from . import models
 
 
-
-
 # Create your views here.
 
 def login_(request):
-    return render(request,"login.html")
+    return render(request, "login.html")
+
 
 def Principal(request):
-    return render(request,"emp_principal.html")
+    return render(request, "emp_principal.html")
+
 
 def Autenticacion_usuarios(request):
     if request.method == 'POST':
@@ -23,13 +23,15 @@ def Autenticacion_usuarios(request):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,user)
-            return redirect('principal') # redirige a la página de inicio después del inicio de sesión exitoso
+            login(request, user)
+            # redirige a la página de inicio después del inicio de sesión exitoso
+            return redirect('principal')
         else:
            # messages.warning(request,"Usuario o contraseña incorrecto, porfavor ingrese de nuevo")
             return render(request, 'login.html', {'error_message': 'Usuario o contraseña incorrecto, porfavor ingrese de nuevo'})
     else:
         return render(request, 'login.html')
+
 
 def guardar_socio(request):
     if request.method == 'POST':
@@ -55,15 +57,15 @@ def guardar_socio(request):
 
         # Convertir la foto a una secuencia de bytes
         foto_bytes = foto.read() if foto else None
-        #foto = request.FILES.get('foto')  # Si se está enviando un archivo
+        # foto = request.FILES.get('foto')  # Si se está enviando un archivo
 
         # Crear una instancia del modelo Socio y guardar los datos en la base de datos
-        socio = models.Socios(usuario=usuario,password=password,cedula=cedula, nombres=nombres, apellidos=apellidos, fecha_nacimiento=fecha_nacimiento,
-                      lugar_nacimiento=lugar_nacimiento, correo_electronico=correo_electronico,
-                      numero_telefonico=numero_telefonico, numero_convencional=numero_convencional,
-                      direccion_domiciliaria=direccion_domiciliaria, facultad=facultad, categoria=categoria,
-                      dedicacion_academica=dedicacion_academica, fecha_ingreso=fecha_ingreso, titulo=titulo, aporte=aporte,
-                      foto=foto_bytes)
+        socio = models.Socios(usuario=usuario, password=password, cedula=cedula, nombres=nombres, apellidos=apellidos, fecha_nacimiento=fecha_nacimiento,
+                              lugar_nacimiento=lugar_nacimiento, correo_electronico=correo_electronico,
+                              numero_telefonico=numero_telefonico, numero_convencional=numero_convencional,
+                              direccion_domiciliaria=direccion_domiciliaria, facultad=facultad, categoria=categoria,
+                              dedicacion_academica=dedicacion_academica, fecha_ingreso=fecha_ingreso, titulo=titulo, aporte=aporte,
+                              foto=foto_bytes)
 
         # Guardar el objeto Socio en la base de datos
         socio.save()
@@ -75,10 +77,9 @@ def guardar_socio(request):
     return render(request, 'emp_agg_socios.html')
 
 
-
 def editar_socio(request, socio_id):
     socio = get_object_or_404(models.Socios, id=socio_id)
-    
+
     if request.method == 'POST':
         socio.usuario = request.POST.get('usuario')
         socio.password = request.POST.get('password')
@@ -90,7 +91,8 @@ def editar_socio(request, socio_id):
         socio.correo_electronico = request.POST.get('correo_electronico')
         socio.numero_telefonico = request.POST.get('numero_telefonico')
         socio.numero_convencional = request.POST.get('numero_convencional')
-        socio.direccion_domiciliaria = request.POST.get('direccion_domiciliaria')
+        socio.direccion_domiciliaria = request.POST.get(
+            'direccion_domiciliaria')
         socio.facultad = request.POST.get('facultad')
         socio.categoria = request.POST.get('categoria')
         socio.dedicacion_academica = request.POST.get('dedicacion_academica')
@@ -101,41 +103,69 @@ def editar_socio(request, socio_id):
 
         # Convertir la foto a una secuencia de bytes
         foto_bytes = socio.foto.read() if socio.foto else None
-        #foto = request.FILES.get('foto')  # Si se está enviando un archivo
+        # foto = request.FILES.get('foto')  # Si se está enviando un archivo
         # Guardar el objeto Socio en la base de datos
         socio.save()
 
-        messages.success(request,'Se ha guardado correctamente este socio')
+        messages.success(request, 'Se ha guardado correctamente este socio')
         return redirect('listar_socios')
     else:
         return render(request, 'emp_edit_socios.html', {'socio': socio})
 
-    
 
 def eliminar_socio(request, socio_id):
     socio = get_object_or_404(models.Socios, id=socio_id)
-    
+
     if request.method == 'GET':
         # Cambiar el estado del socio a inactivo
         socio.activo = False
         socio.save()
 
         # Agregar un mensaje de confirmación
-        messages.success(request, 'El socio ha sido dado de baja exitosamente.')
-       
-    else: 
+        messages.success(
+            request, 'El socio ha sido dado de baja exitosamente.')
+
+    else:
         messages.warning(request, 'El socio no pudo ser eliminado')
 
     return redirect('listar_socios')
 
 
+def buscar_socios(request):
+
+    criterio = request.GET.get('criterio')
+    valor = request.GET.get('valor')
+    socios = []
+
+    if criterio:
+        # Filtrar socios según el criterio y el valor ingresados
+        if criterio == 'nombres':
+            socios = models.Socios.objects.filter(nombres__icontains=valor)
+        elif criterio == 'apellidos':
+            socios = models.Socios.objects.filter(apellidos__icontains=valor)
+        elif criterio == 'categoria':
+            socios = models.Socios.objects.filter(categoria__icontains=valor)
+        elif criterio == 'facultad':
+            socios = models.Socios.objects.filter(facultad__icontains=valor)
+        elif criterio == 'cedula':
+            socios = models.Socios.objects.filter(cedula__icontains=valor)
+        elif criterio == '1':
+            socios = models.Socios.objects.filter(activo__icontains=criterio)
+        elif criterio == '0':
+            socios = models.Socios.objects.filter(activo__icontains=criterio)
+       
+    
+    return render(request, 'emp_socios.html', {'socios': socios})
+
+
 def Recuperar_cuenta(request):
-    return render(request,"recuperar_contra.html")
+    return render(request, "recuperar_contra.html")
+
 
 def ListaSocios(request):
     socios = models.Socios.objects.all()
-    return render(request,"emp_socios.html", {'socios': socios})
+    return render(request, "emp_socios.html", {'socios': socios})
+
 
 def AggSocio(request):
-    return render(request,"emp_agg_socios.html")
-
+    return render(request, "emp_agg_socios.html")
