58f5e11ee584071913abaec7a69ce6c03bafc4b0
vmolinam7 <victor.molina2017@uteq.edu.ec>
2023-08-27 20:54:44 -0500

    cambios vm

--- pagos/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Pagos
-
-class PagosForm(forms.ModelForm):
-    class Meta:
-        model = Pagos
-        fields=['socio', 'proveedor','consumo_total', 'fecha_consumo']
-        
