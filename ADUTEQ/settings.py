d181d43bb5dbe28f9275aa281c3b639c80aa7f9a
vmolinam7 <victor.molina2017@uteq.edu.ec>
2023-09-15 15:00:36 -0500

    tercerT

--- ADUTEQ/settings.py
+++ ADUTEQ/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'ADUTEQ.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:root@localhost:5432/dbaduteq',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
