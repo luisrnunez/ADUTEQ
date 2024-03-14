89bfc4bbe204285c0f0bc5e22fffdf18d0e0e032
vmolinam7 <victor.molina2017@uteq.edu.ec>
2024-03-14 17:58:19 -0500

    Version Final hasta ahora

--- ADUTEQ/settings.py
+++ ADUTEQ/settings.py
@@ -202,8 +202,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'prctcorreo@gmail.com'
-EMAIL_HOST_PASSWORD = 'pnrriqutcefjsqnd'
+EMAIL_HOST_USER = 'notificacionesaduteq@gmail.com'
+EMAIL_HOST_PASSWORD = 'hxid xdn k xvw eaoe'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
